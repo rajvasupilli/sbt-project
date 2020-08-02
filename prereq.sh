a6abfc08860e8e23af741d4be5dca5234615278f
root <root@ip-172-31-70-100.ec2.internal>
2020-08-02 06:40:31 +0000

    test

--- prereq.sh
+++ prereq.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 # This script installs all the prequisites needed.
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y gnupg2
+apt-get install -y gnupg2
 
-sudo apt-get install -y wget
+apt-get install -y wget
 
-sudo apt-get install -y curl
+apt-get install -y curl
 
-sudo apt-get -y install openjdk-8-jdk
+apt-get -y install openjdk-8-jdk
 
 echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
 
-sudo curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
 
-sudo apt-get update
+apt-get update
 
 apt-get install -y sbt
 
