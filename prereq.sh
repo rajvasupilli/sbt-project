6d614ec941a30188e7e385a37f536b9002e14564
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2020-08-02 12:23:06 +0530

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -11,9 +11,9 @@ sudo apt-get install -y curl
 
 sudo apt-get -y install openjdk-8-jdk
 
-echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
+sudo curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | apt-key add
 
 sudo apt-get update
 
